d642a200d684ffda00104101b9e928c52a4bd8f0
wyf-2019 <53163090+wyf-2019@users.noreply.github.com>
2019-10-31 14:54:40 +0800

    Create SC-K00A.vb

---
+++ 03_コーディング/Master/ProcessManagement/form/SC-K00A.vb
@@ -0,0 +1,30 @@
+﻿Public Class SC_K00A
+
+    Private Const FORM_NAME As String = "Achievement reference menu(実績参照メニュー)"
+
+    ''' <summary>
+    ''' 初期表示
+    ''' </summary>
+    Private Sub SC_K00A_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        lblMaster.Text = FORM_NAME
+        Me.Text = "[" & Me.Name & "]" & FORM_NAME
+
+    End Sub
+
+    ''' <summary>
+    ''' GroupBox設定
+    ''' </summary>
+    Private Sub GroupBox2_Paint(sender As Object, e As PaintEventArgs) Handles GroupBox2.Paint
+        Dim gBox As GroupBox = sender
+        e.Graphics.Clear(gBox.BackColor)
+        e.Graphics.DrawString(gBox.Text, gBox.Font, Brushes.Black, 7, 1)
+        Dim vSize = e.Graphics.MeasureString(gBox.Text, gBox.Font)
+        e.Graphics.DrawLine(Pens.Black, 1, vSize.Height / 2, 8, vSize.Height / 2)
+        e.Graphics.DrawLine(Pens.Black, vSize.Width + 8, vSize.Height / 2, gBox.Width - 2, vSize.Height / 2)
+        e.Graphics.DrawLine(Pens.Black, 1, vSize.Height / 2, 1, gBox.Height - 2)
+        e.Graphics.DrawLine(Pens.Black, 1, gBox.Height - 2, gBox.Width - 2, gBox.Height - 2)
+        e.Graphics.DrawLine(Pens.Black, gBox.Width - 2, vSize.Height / 2, gBox.Width - 2, gBox.Height - 2)
+    End Sub
+
+End Class
