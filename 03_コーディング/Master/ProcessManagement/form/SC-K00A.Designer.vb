54864358843b0c36cac950ef1aeac7f6cab8a979
wyf-2019 <53163090+wyf-2019@users.noreply.github.com>
2019-10-31 14:54:32 +0800

    Create SC-K00A.Designer.vb

---
+++ 03_コーディング/Master/ProcessManagement/form/SC-K00A.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SC_K00A
+    Inherits ProcessManagement.ParentTemplate
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnManagement = New System.Windows.Forms.Button()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.Window
+        Me.GroupBox2.Controls.Add(Me.Button8)
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button5)
+        Me.GroupBox2.Controls.Add(Me.Button6)
+        Me.GroupBox2.Controls.Add(Me.Button7)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.btnManagement)
+        Me.GroupBox2.Font = New System.Drawing.Font("MS UI Gothic", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 159)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(1327, 437)
+        Me.GroupBox2.TabIndex = 272
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Achievement reference menu(実績参照メニュー)"
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Button8.Location = New System.Drawing.Point(694, 61)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(270, 49)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "  Achievement reference by period  期間別実績照会(K-15)"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Button4.Location = New System.Drawing.Point(36, 317)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(270, 49)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "        Other issues refer ・ cancel        その他出庫参照・取消(K-21)"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Button5.Location = New System.Drawing.Point(1019, 194)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(270, 49)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "                 Other issues                   " & Global.Microsoft.VisualBasic.ChrW(9) & "その他出庫(K-20)"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Button6.Location = New System.Drawing.Point(694, 194)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(270, 49)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "        Achievement management        実績管理(K-19)"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Button7.Location = New System.Drawing.Point(369, 194)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(270, 49)
+        Me.Button7.TabIndex = 4
+        Me.Button7.Text = "    Assembly progress management    組立進度管理(K-18)"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Button3.Location = New System.Drawing.Point(36, 194)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(270, 49)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "        Defect analysis by mold          成形金型別不良分析(K-17)"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Button2.Location = New System.Drawing.Point(1019, 61)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(270, 49)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "    Molding achievement reference    成形実績参照(K-16)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Button1.Location = New System.Drawing.Point(369, 61)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(270, 49)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "The results before the previous days前日以前実績参照(K-14)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnManagement
+        '
+        Me.btnManagement.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.btnManagement.Location = New System.Drawing.Point(36, 61)
+        Me.btnManagement.Name = "btnManagement"
+        Me.btnManagement.Size = New System.Drawing.Size(270, 49)
+        Me.btnManagement.TabIndex = 0
+        Me.btnManagement.Text = "            The results on today              当日実績参照(K-13)"
+        Me.btnManagement.UseVisualStyleBackColor = True
+        '
+        'SC_K00A
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.ClientSize = New System.Drawing.Size(1354, 659)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Name = "SC_K00A"
+        Me.Controls.SetChildIndex(Me.GroupBox2, 0)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnManagement As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+End Class
