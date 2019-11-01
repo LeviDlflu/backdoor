1c05f52b1235502bedc0f77dbcecbf987b79fa9d
wyf-2019 <53163090+wyf-2019@users.noreply.github.com>
2019-11-01 10:00:29 +0800

    Update SC-K00A.vb

--- 03_コーディング/Master/ProcessManagement/form/SC-K00A.vb
+++ 03_コーディング/Master/ProcessManagement/form/SC-K00A.vb
@@ -27,4 +27,64 @@
         e.Graphics.DrawLine(Pens.Black, gBox.Width - 2, vSize.Height / 2, gBox.Width - 2, gBox.Height - 2)
     End Sub
 
+    ''' <summary>
+    ''' 当日実績参照(K-13)
+    ''' </summary>
+    Private Sub btnManagement_Click(sender As Object, e As EventArgs) Handles btnManagement.Click
+
+        Dim frm As New SC_K13()
+        frm.ShowDialog()
+        Me.Show()
+    End Sub
+
+    ''' <summary>
+    ''' 前日以前実績参照(K-14)
+    ''' </summary>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim frm As New SC_K14()
+        frm.ShowDialog()
+        Me.Show()
+
+    End Sub
+
+    ''' <summary>
+    ''' 成形実績参照(K-16)
+    ''' </summary>
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim frm As New SC_K16()
+        frm.ShowDialog()
+        Me.Show()
+    End Sub
+
+    ''' <summary>
+    ''' 成形金型別不良分析(K-17)
+    ''' </summary>
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim frm As New SC_K17()
+        frm.ShowDialog()
+        Me.Show()
+    End Sub
+
+    ''' <summary>
+    ''' その他出庫(K-20)
+    ''' </summary>
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Dim frm As New SC_K20()
+        frm.ShowDialog()
+        Me.Show()
+    End Sub
+
+    ''' <summary>
+    ''' その他出庫参照・取消(K-21)
+    ''' </summary>
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim frm As New SC_K21()
+        frm.ShowDialog()
+        Me.Show()
+    End Sub
 End Class
