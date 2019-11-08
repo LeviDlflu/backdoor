752f23806e735d6901398a19b2b171d0ff905364
wyf-2019 <53163090+wyf-2019@users.noreply.github.com>
2019-11-08 17:55:34 +0800

    Update ParentTemplate.vb

--- 03_コーディング/Master/ProcessManagement/form/ParentTemplate.vb
+++ 03_コーディング/Master/ProcessManagement/form/ParentTemplate.vb
@@ -1,6 +1,61 @@
-﻿Public Class ParentTemplate
+﻿Imports System.Runtime.InteropServices
+
+Public Class ParentTemplate
 
     Private Const CONST_SYSTEM_NAME As String = "B／D生産管理システム"
+
+    Structure Parameter
+        ''' <summary>
+        ''' 工程
+        ''' </summary>
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)>
+        Dim Process As String
+
+        ''' <summary>
+        ''' 部品番号
+        ''' </summary>
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)>
+        Dim PartNumber As String
+
+        ''' <summary>
+        ''' 品名略称
+        ''' </summary>
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)>
+        Dim ProductName As String
+
+        ''' <summary>
+        ''' パック品名略称
+        ''' </summary>
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)>
+        Dim PackProductName As String
+
+        ''' <summary>
+        ''' 納入先コード
+        ''' </summary>
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)>
+        Dim DeliveryCode As String
+
+        ''' <summary>
+        ''' 納入区分
+        ''' </summary>
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)>
+        Dim DeliveryDivision As String
+
+        ''' <summary>
+        ''' 製品半製品区分
+        ''' </summary>
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)>
+        Dim SemiFinishedProductDivision As String
+
+        ''' <summary>
+        ''' 品名事業所コード
+        ''' </summary>
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=32)>
+        Dim ProductNamePlantCode As String
+
+    End Structure
+
+    Public Shared formParameter As New Parameter
 
     Private Sub TimeSys_Tick(sender As Object, e As EventArgs) Handles TimeSys.Tick
         slblDay.Text = Format(Now, "yyyy/MM/dd")
