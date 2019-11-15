ef7231023d01ac3903f7234e8134a56f3995c01e
wyf-2019 <53163090+wyf-2019@users.noreply.github.com>
2019-11-15 11:56:25 +0800

    ユーザーコントロールを追加

---
+++ 03_コーディング/Master/ProcessManagement/UserControl/CustomDateTimePicker.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CustomDateTimePicker
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl はコンポーネント一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Yellow
+        Me.TextBox1.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.MaximumSize = New System.Drawing.Size(110, 21)
+        Me.TextBox1.MinimumSize = New System.Drawing.Size(110, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(110, 21)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "yyyy/MM/dd"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "yyyy/MM/dd"
+        Me.DateTimePicker1.DropDownAlign = System.Windows.Forms.LeftRightAlignment.Right
+        Me.DateTimePicker1.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(107, 0)
+        Me.DateTimePicker1.MaximumSize = New System.Drawing.Size(18, 21)
+        Me.DateTimePicker1.MinimumSize = New System.Drawing.Size(18, 21)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(18, 21)
+        Me.DateTimePicker1.TabIndex = 2
+        '
+        'CustomDateTimePicker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Window
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Name = "CustomDateTimePicker"
+        Me.Size = New System.Drawing.Size(145, 21)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+End Class
---
+++ 03_コーディング/Master/ProcessManagement/UserControl/CustomDateTimePicker.vb
@@ -0,0 +1,9 @@
+﻿Public Class CustomDateTimePicker
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        TextBox1.Text = DateTimePicker1.Text
+    End Sub
+
+    Private Sub CustomDateTimePicker_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = DateTimePicker1.Text
+    End Sub
+End Class
